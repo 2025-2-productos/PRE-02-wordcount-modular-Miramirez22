08b667aec544a833d99d48287f58bcd603700f8e
Miramirez22 <miramirez@unal.edu.co>
2025-11-22 11:04:53 -0500

    Update

--- homework/src/_internals/read_all_lines.py
+++ homework/src/_internals/read_all_lines.py
@@ -1,10 +1,10 @@
 import os
 
 
-def read_all_lines():
+def read_all_lines(input_folder):
     all_lines = []
-    input_files_list = os.listdir("data/input/")
+    input_files_list = os.listdir(input_folder)
     for filename in input_files_list:
-        with open(os.path.join("data/input/", filename), "r", encoding="utf-8") as f:
+        with open(os.path.join(input_folder, filename), "r", encoding="utf-8") as f:
             all_lines.extend(f.readlines())
     return all_lines
---
+++ homework/src/_internals/write_word_counts.py
@@ -0,0 +1,12 @@
+import os
+
+
+def write_word_counts(counter, output_folder):
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # save the results using tsv format
+    with open(os.path.join(output_folder, "wordcount.tsv"), "w", encoding="utf-8") as f:
+        for key, value in counter.items():
+            # write the key and value to the file
+            f.write(f"{key}\t{value}\n")
